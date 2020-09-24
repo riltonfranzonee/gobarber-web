6c1b95733590acab64886a1ff3ed5a4f2c6e4d84
Rilton Franzone <riltonfranzone@gmail.com>
2020-09-24 20:37:54 -0300

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,5 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => <h1>Hello world</h1>;
 
 export default App;
